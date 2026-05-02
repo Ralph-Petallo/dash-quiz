e6c0e8d8a5286ce3af3f7193aa0c51d98854d693
ralphpetallo <ralphpetallo@gmail.com>
2026-05-02 19:48:09 +0800

    add mobile port for api request

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:8000', 'http://127.0.0.1:5173'],
+    'allowed_origins' => ['http://127.0.0.1:8000', 'http://127.0.0.1:5173', 'http://localhost:8081'],
 
     'allowed_origins_patterns' => [],
-
+    
     'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
