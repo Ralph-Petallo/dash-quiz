9cce33d944d2c493c143e504a44ef370d985ac26
ralphpetallo <ralphpetallo@gmail.com>
2026-09-19 17:52:19 +0800

    fix: incorrect use of asset()

--- app/Http/Controllers/Api/ImageIdentificationAssessmentController.php
+++ app/Http/Controllers/Api/ImageIdentificationAssessmentController.php
@@ -16,9 +16,10 @@ class ImageIdentificationAssessmentController extends Controller
             ->take(10)
             ->get()
             ->each(function ($question) {
-                $question->image_path = asset('storage/images/images_identification' . $question->image_path);
+                $question->image_path = asset(
+                    'images/images_identification/' . ltrim($question->image_path, '/')
+                );
             });
-
 
         if ($questions->isEmpty()) {
             return response()->json([
