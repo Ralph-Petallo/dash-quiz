312b34f1c11c64ece6c84c46d401a7be4bf7f0ee
ralphpetallo <ralphpetallo@gmail.com>
2026-09-19 17:50:59 +0800

    feat: added assessment table (migration)

--- database/migrations/2026_09_16_060635_create_assessments_table.php
+++ database/migrations/2026_09_16_060635_create_assessments_table.php
@@ -4,22 +4,29 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('assessments', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
             $table->id();
+
+            $table->foreignId('coc_id')
+                ->constrained('quizzes')
+                ->cascadeOnDelete();
+
+            $table->string('type');
             $table->string('title');
-            $table->string('description');
-            $table->string('component');
-            $table->string('icon');
-            $table->string('difficulty');
+            $table->string('icon')->nullable();
+            $table->text('description')->nullable();
+            $table->string('category')->nullable();
+            $table->string('difficulty')->nullable();
+            $table->string('meta')->nullable();
             $table->string('path');
+            $table->boolean('available')->default(true);
+
             $table->timestamps();
         });
     }
