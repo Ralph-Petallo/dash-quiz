fe125ec2cbdd8277d2bb0e21a06e134811cd59ae
ralphpetallo <ralphpetallo@gmail.com>
2026-09-21 23:40:40 +0800

    feat: added tab icon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CSS Prep</title>
-    <link rel="icon" href="{{ asset('bolt.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('lightning.png') }}" type="image/x-icon">
     @vite(['resources/js/app.js'])
 </head>
 
